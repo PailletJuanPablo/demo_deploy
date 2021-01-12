89ad38710dc98950dddae2f32aeceb9e8bead665
Juan Pablo Paillet <pailletjuanpablo@gmail.com>
2021-01-12 17:52:49 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+
+## Ejercitación Formularios
+
+ 
+Ejecutar
+
+    npm start $NUMEROPUERTO
+
+para levantar el servidor, por ejemplo:
+
+    npm start 4500
+Para que el servidor comience en el puerto 4500.
+Si no se especifica ningún puerto, o no se envía un número como argumento, se ejecutará en el puerto 3000 por defecto.
---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+
+// Método para formatear el puerto en base al argumento enviado en npm start
+const formattedPort = (() => {
+    // se destructura el array para que tome la tercera posición 
+    // (process.argv es un array que tiene como primer index el PATH de node y como segundo índice el archivo)
+    let [,,portNumber] = process.argv;
+    // Si el argumento del puerto es enviado y al parsearlo a número no es NAN, se devuelve
+    // Caso contrario el puerto por defecto es 3000
+    return portNumber && !isNaN(parseInt(portNumber)) ? parseInt(portNumber) : 3000;
+})()
+
+// Se define la carpeta pública para exponer assets y archivos estáticos
+app.use(express.static('public'));
+
+// Se levanta el servidor en el resultado de parsear el puerto desde el argumento
+app.listen(process.env.port || 3000 , () =>{
+    console.log('Servidor funcionando en puerto ' + formattedPort);
+});
+
+// Se definen rutas y respuestas en el mismo script (recordar que esto puede modularizarse)
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
